0f3a389bf1483d0ac38b4390efd1ff1dc2cef99a
Noah Foroma <noah@Noahs-MacBook-Pro.local>
2025-07-21 21:04:35 +0200

    chore: added validation and null checking; updates to test pack

--- src/RecipeShare.Library/BusinessLogic/Implementations/RecipeShareBusinessLogic.cs
+++ src/RecipeShare.Library/BusinessLogic/Implementations/RecipeShareBusinessLogic.cs
@@ -21,30 +21,68 @@ public class RecipeShareBusinessLogic : IRecipeShareBusinessLogic
 
     public async Task<RecipeResponse?> GetRecipeById(int id, CancellationToken cancellationToken)
     {
+        if (id is < 0 or > int.MaxValue)
+        {
+            _logger.LogWarning("Invalid recipe ID: {Id}", id);
+            return null;
+        }
+        
         _logger.LogInformation("Getting recipe with ID {Id}", id);
         
-        return await _recipeShareRepository.GetRecipeById(id, cancellationToken);
+        var recipe = await _recipeShareRepository.GetRecipeById(id, cancellationToken);
+        
+        if (recipe is not null) return recipe;
+        
+        _logger.LogWarning("No recipe found with ID {Id}", id);
+        return null;
     }
     
     public async Task<RecipeResponse?> GetRecipeByTitle(string title, CancellationToken cancellationToken)
     {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            _logger.LogWarning("Invalid recipe title");
+            return null;
+        }
+        
         _logger.LogInformation("Getting recipe with title {Title}", title);
         
-        return await _recipeShareRepository.GetRecipeByTitle(title, cancellationToken);
+        var recipe = await _recipeShareRepository.GetRecipeByTitle(title, cancellationToken);
+        
+        if (recipe is not null) return recipe;
+        
+        _logger.LogWarning("No recipe found with title {Title}", title);
+        return null;
     }
 
     public async Task<IEnumerable<RecipeResponse?>> GetRecipes(CancellationToken cancellationToken)
     {
         _logger.LogInformation("Getting recipes");
         
-        return await _recipeShareRepository.GetRecipes(cancellationToken);
+        var recipes = await _recipeShareRepository.GetRecipes(cancellationToken);
+        
+        if (recipes is not null && recipes.Any()) return recipes;
+        
+        _logger.LogWarning("No recipes found");
+        return [];
     }
 
     public async Task<IEnumerable<RecipeResponse?>> GetRecipesByDietaryTag(string dietaryTag,
         CancellationToken cancellationToken)
     {
+        if (string.IsNullOrWhiteSpace(dietaryTag))
+        {
+            _logger.LogWarning("Invalid dietary tag");
+            return [];
+        }
         _logger.LogInformation("Getting recipes with dietary tag {DietaryTag}", dietaryTag);
         
-        return await _recipeShareRepository.GetRecipeByDietaryTag(dietaryTag, cancellationToken);
+        var recipes = await _recipeShareRepository.GetRecipeByDietaryTag(dietaryTag, cancellationToken);
+
+        if (recipes is not null && recipes.Any()) return recipes;
+        
+        _logger.LogWarning("No recipes found with dietary tag {DietaryTag}", dietaryTag);
+        return [];
+
     }
 }
